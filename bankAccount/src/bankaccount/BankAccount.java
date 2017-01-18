64fc390d653461a5065c298d8e13221053d29edb
franc <franc@Pacopa-MSI>
2017-01-17 21:59:06 -0600

    Bank Account

--- bankAccount/src/bankaccount/BankAccount.java
+++ bankAccount/src/bankaccount/BankAccount.java
@@ -4,28 +4,28 @@ package bankaccount;
 /**
  * @author franc
  */
-public class BankAccount {
+public class BankAccount { //Se definen las propiedades de la cuenta
     public String name;
     public int accNum;
     public double balance;
     private double deposit;
     private double withdraw;
     
-public BankAccount(){
+public BankAccount(){  //Se establecen los valores iniciales de la cuenta
     name = "Francisco Piñeiro";
     accNum= 0001;
     balance= 1500.25;
 }
 
-public double deposit(){
+public double deposit(){ //Metodo para hacer un deposito a la cuenta
     return this.deposit;
 }
 
-public double withdraw(){
+public double withdraw(){ //Metodo para hacer un retiro a la cuenta
     return this.withdraw;
 }
 
-public double balance(){
+public double balance(){ //Metodo para obtener el saldo de la cuenta
     return this.balance;
 }
     
