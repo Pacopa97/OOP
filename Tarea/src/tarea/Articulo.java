db00473ce32792c6dd372483a07ae2c0bff6c47c
franc <franc@Pacopa-MSI>
2017-01-27 10:52:45 -0600

    Tarea

---
+++ Tarea/src/tarea/Articulo.java
@@ -0,0 +1,88 @@
+package tarea;
+
+/**
+ *
+ * @author franc
+ */
+public class Articulo {
+    
+    public int SKU;
+    private int existencia;
+    private double precio;
+    private double costo;
+    public String descripcion;
+    
+    public Articulo(int SKU,double costo,double precio,int existencia,String descripcion){
+        this.SKU = SKU;
+        this.costo = costo;
+        this.precio= precio;
+        this.existencia=existencia;
+        this.descripcion=descripcion;   
+    }
+    
+    public Articulo(){
+     this(0,0.00,0.00,0,"Sin descripcion");   
+    }
+      public int getSKU() {
+        return SKU;
+    }
+    
+      public void setSKU(int SKU){
+            this.SKU = SKU;
+        }
+      
+      public int getExistencia() {
+        return existencia;
+    }
+    
+      public void setExistencia(int existencia){
+            this.existencia = existencia;
+        }
+      public double setCosto(double costo) {
+        return costo;
+    }
+    
+      public void getCosto(){
+            this.costo =costo;
+        }
+     
+      public double getPrecio(){
+          return precio;
+      }
+      
+      public void setPrecio(double precio){
+          this.precio = precio;
+      }
+    
+      public String getDescripcion(){
+          return descripcion;
+      }
+      
+      public void setDescripcion(String description ){
+         this.descripcion = descripcion;
+      }
+    
+    public void compra(int e, double c){
+        setExistencia(e);
+        setCosto(c); 
+    }
+    
+    public boolean venta(int n){
+        if(getExistencia()>=n){
+            setExistencia(n-getExistencia());
+        return true;
+        }else{
+        return false; 
+    }
+    
+    }
+    
+    public void verificarExistencia(){
+        getExistencia();
+    }
+    
+    public void actualizaPrecio(double precio){
+        this.precio=precio;
+        
+    }
+}
